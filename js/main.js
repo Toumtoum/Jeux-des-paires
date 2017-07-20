e030b9b1e2460511900f0b994d992a4c31bec89d
Toumtoum <mehdi.toumia@yahoo.com>
2017-07-20 09:59:35 +0200

    at the stage script able to assign a random back color at each card

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 var card = document.getElementsByClassName('carte');
 
-var colors = ['#5d8aa8','#5d8aa8','#efdecd','#9966cc','#008000','#00ffff','#4b5320','#5d8aa8','#5d8aa8','#efdecd','#9966cc','#008000','#00ffff','#4b5320']
+var colors = ['#5d8aa8','#d2691e','#efdecd','#9966cc','#008000','#00ffff','#4b5320','#5d8aa8','#d2691e','#efdecd','#9966cc','#008000','#00ffff','#4b5320']
 
 var randomColor;
 
@@ -20,17 +20,47 @@ for (var i = 0; i < card.length ; i++){
 }
 }
 
-function assignRandomColors () {
+// function assignRandomColors () {
+//
+//   randomColor = colors[Math.floor(Math.random() * colors.length)];
+//
+//   for (var i = 0; i < randomColor.length ; i++){
+//
+//     card[i].style.backgroundColor = randomColor[i];
+//
+//   };
+// }
 
-  randomColor = Math.floor(Math.random() * colors.length);
-  
+ function schuffle (array){
+
+  var i;
+  var j;
+  var temp;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+function assignColor (){
+
+  for (var i = 0; i < colors.length; i++) {
+
+    card[i].style.backgroundColor = colors[i];
+
+  }
 
 }
 
 
 
 
+
 //----------------------------Script----------------------------------------
 
-revealBackSide ()
-assignRandomColors ()
+revealBackSide ();
+schuffle (colors);
+assignColor ();
